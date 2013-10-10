0e31b09c8dbef10cee9e88bf3898fb8c33b3d9ba
Dave Welch <dave.welch@mokinetworks.com>
2013-10-10 08:53:17 -0600

    Initial Commit

---
+++ mpParse/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mpParse
+//
+//  Created by Dave Welch on 10/10/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
